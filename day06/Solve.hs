54eadd9f0ca192e06fb1d2b125edd3a749997ce0
AlecsFerra <alecsferra@protonmail.com>
2023-12-06 12:30:26 +0100

    Day 6: fixup

--- day06/Solve.hs
+++ day06/Solve.hs
@@ -15,8 +15,8 @@ part2 = uncurry solve
       . bimap num num
   where num = read . concat
 
-solve time distance | even time = (delta2 `div` 2) * 2 + 1
-                    | otherwise = (delta2 + 1)
+solve time distance | even time = delta2       `div` 2 * 2 + 1
+                    | otherwise = (delta2 + 1) `div` 2 * 2
   where
     optimal = time `div` 2
     delta2  = floor $ sqrt (fromIntegral $ time ^ 2 - 4 * distance)
