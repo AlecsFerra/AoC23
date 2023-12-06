4aeaf42134c60880f2cb9847759b05cd8a06a21a
AlecsFerra <alecsferra@protonmail.com>
2023-12-06 12:14:13 +0100

    Day 6: Closed form

--- day06/Solve.hs
+++ day06/Solve.hs
@@ -7,29 +7,22 @@ parse = ((!! 0) &&& (!! 1))
       . fmap (tail . words)
       . lines
 
-part1 = product 
-      . fmap (uncurry solve . bimap read read) 
+part1 = product
+      . fmap (uncurry solve . bimap read read)
       . uncurry zip
 
-part2 = uncurry solve 
+part2 = uncurry solve
       . bimap num num
   where num = read . concat
 
-solve time distance = search ok         max time
-                    - search (not . ok) 0   max
+solve time distance | even time = (delta2 `div` 2) * 2 + 1
+                    | otherwise = (delta2 + 1)
   where
-    mid x y = x + (y - x) `div` 2
-    max = time `div` 2
-    ok hold = (time - hold) * hold > distance
-
-    -- equiv max $ filter p [l .. h]
-    search p l h | l + 1 == h  = l
-                 | p mid       = search p mid h
-                 | otherwise   = search p l mid
-      where mid = l + (h - l) `div` 2
+    optimal = time `div` 2
+    delta2  = floor $ sqrt (fromIntegral $ time ^ 2 - 4 * distance)
 
 main = do
-  input <-  parse <$> readFile "input.txt" :: IO ([String], [String])
+  input <-  parse <$> readFile "input.txt"
   print $ part1 input
   print $ part2 input
   pure ()
