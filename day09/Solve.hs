bc59c97d146f268de7acd6fb530420e21eff6116
AlecsFerra <alecsferra@protonmail.com>
2023-12-09 14:08:17 +0100

    Day 9: better

--- day09/Solve.hs
+++ day09/Solve.hs
@@ -2,15 +2,15 @@ module Solve (main) where
 
 import Data.List (tails, foldl')
 import Control.Arrow ((&&&))
+import Control.Monad (ap)
 
 parse = fmap (fmap (read @Int) . words) . lines
 
 part1 = sum . fmap (foldl' (+) 0 . fmap last . sequences)
-  where sequences = takeWhile (not . all (== 0)) 
+  where sequences = takeWhile (not . all (== 0))
                   . iterate step
-        step = fmap (uncurry (flip (-)) . ((!! 0) &&& (!! 1)))
-             . filter ((>= 2) . length)
-             . tails
+        step = fmap (uncurry (flip (-)))
+             . ap zip tail
 
 part2 = part1 . fmap reverse
 
